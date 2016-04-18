407344b7807db15162a32bec15ba49205cd61742
jsegovia <jsegovia@squareballstudios.com>
2016-04-18 15:44:37 -0500

    xamarin issue. binding list of string array. it does not work on a real device ios 9.2

---
+++ test/MyPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace test
+{
+	public class MyPage : ContentPage
+	{
+		ListView list;
+		public MyPage ()
+		{
+			List<string[]> array = new List<string[]>(){
+				new string[]{"1", "hello"},
+				new string[]{"2", "world"},
+				new string[]{"3", "hello"}
+			};
+
+			list=new ListView();
+
+			var cell = new DataTemplate (typeof(TextCell));
+			cell.SetBinding (TextCell.TextProperty,".[1]");
+			list.ItemTemplate = cell;
+
+			list.ItemsSource = array;
+
+			Content = list;
+		}
+	}
+}
+
+
